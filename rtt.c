91c62cd0dca6b5eb1a8174ed16f577e95047a28d
Ubuntu <ubuntu@ip-172-31-8-226.ec2.internal>
2015-10-14 13:03:01 +0000

    rtt simulation

---
+++ rtt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    double EstimatedRTT = 90.0;
+    double DevRTT = 25;
+    double TimeoutInterval;
+    double a = 0.1;
+    double b = 0.25;
+    for (int i = 0; i < 100; ++i) {
+        EstimatedRTT = (1-a)*EstimatedRTT + a*200.0;
+        DevRTT = (1-b)*DevRTT + b*abs(200.0 - EstimatedRTT);
+        TimeoutInterval = EstimatedRTT + 4.0*DevRTT;
+        printf("After #%d measured, TimeoutInterval: %f\n", i, TimeoutInterval);
+    }
+    return 0;
+}
